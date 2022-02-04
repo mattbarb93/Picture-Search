2e7305daa34e65e713ab55ef74d625f6b0ea67d4
Matheus Barbosa <mattbarb93@gmail.com>
2022-02-04 13:39:09 -0500

    SearchBar component onFormSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  //Controlled element
+  state = { term: "" };
+
+  //To solve "this" undefined issue, make the object into an arrow funtion
+  onFormSubmit = (event) => {
+    event.preventDefault(); //Prevents form to automatically be submitted and the browser from reloading
+
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
