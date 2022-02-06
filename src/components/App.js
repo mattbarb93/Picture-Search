0403720836b40185468c623c3d5bc867f6786f51
Matheus Barbosa <mattbarb93@gmail.com>
2022-02-05 23:42:24 -0500

    First API set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
+/*
+    BREAKDOWN OF API CALL
+
+    1) Using Axios, use axios.get('[API URL]', {
+        params refer to the query string parameters we will use. we'll add query, and the value will be the term that was passed into the onSearchSubmit function. That will add a string to the API URL, making a request.
+        headers object allows us to add headers to the request to the API 
+    })
+*/
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 8ec0d1939461028706d51f288feb8dde7c68ad9a45fbb7b4ad1c1f9d5e1da8dc",
+      },
+    });
   }
 
   render() {
