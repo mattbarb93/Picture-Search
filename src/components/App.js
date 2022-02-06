c50fb396f65b3e3bb8394ff5eed459133c846387
Matheus Barbosa <mattbarb93@gmail.com>
2022-02-05 23:51:46 -0500

    .then syntax

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,25 @@ import SearchBar from "./SearchBar";
     BREAKDOWN OF API CALL
 
     1) Using Axios, use axios.get('[API URL]', {
-        params refer to the query string parameters we will use. we'll add query, and the value will be the term that was passed into the onSearchSubmit function. That will add a string to the API URL, making a request.
-        headers object allows us to add headers to the request to the API 
     })
+     - params refer to the query string parameters we will use. we'll add query, and the value will be the term that was passed into the onSearchSubmit function. That will add a string to the API URL, making a request.
+     - headers object allows us to add headers to the request to the API 
+    2) add .then at the end of the .get request, so a callback is invoked at some point in the future, with whatever data we got from the Splash API.
 */
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID 8ec0d1939461028706d51f288feb8dde7c68ad9a45fbb7b4ad1c1f9d5e1da8dc",
-      },
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID 8ec0d1939461028706d51f288feb8dde7c68ad9a45fbb7b4ad1c1f9d5e1da8dc",
+        },
+      })
+      .then((response) => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
